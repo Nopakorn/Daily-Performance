7697cc876ac24597e7e13b92a87c91ea667aa258
Nopakorn <guilrockhopper@gmail.com>
2015-04-08 11:27:08 +0700

    initial project code

---
+++ Daily-Performance/Daily-Performance/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Daily_Performance.Startup))]
+namespace Daily_Performance
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

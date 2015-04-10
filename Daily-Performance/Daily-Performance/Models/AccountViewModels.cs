79adc23993edf08fa67ec62ea490427603710e65
Nopakorn <guilrockhopper@gmail.com>
2015-04-10 12:09:02 +0700

    changing password validation, adding table UI, routing to Login panel

--- Daily-Performance/Daily-Performance/Models/AccountViewModels.cs
+++ Daily-Performance/Daily-Performance/Models/AccountViewModels.cs
@@ -69,6 +69,12 @@ namespace Daily_Performance.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+
+        [Required]
+        [Display(Name = "User")]
+        public string UserName { get; set; }
+
+
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
